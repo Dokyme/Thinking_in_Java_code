45d98df89d422465d9b8af49323db8d5d7663766
ZouDikai <zdk_cz@sina.com>
2017-02-01 19:48:15 +0800

    finish chapter 5

---
+++ src/initialization/SimpleEnumUse.java
@@ -0,0 +1,11 @@
+package initialization;
+
+/**
+ * Created by zdk on 17-2-1.
+ */
+public class SimpleEnumUse {
+    public static void main(String[] args) {
+        Spiciness howHot = Spiciness.MEDIUM;
+        System.out.println(howHot);
+    }
+}
---
+++ src/initialization/Spiciness.java
@@ -0,0 +1,8 @@
+package initialization;
+
+/**
+ * Created by zdk on 17-2-1.
+ */
+public enum Spiciness{
+    NOT,MILD,MEDIUM,HOT,FLAMING
+}
