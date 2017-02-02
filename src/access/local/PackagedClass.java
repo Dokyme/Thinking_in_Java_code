43ecb27b9ff9233444a54150553941286e938ba3
ZouDikai <zdk_cz@sina.com>
2017-02-02 20:07:55 +0800

    to 7.1

--- src/access/local/PackagedClass.java
+++ src/access/local/PackagedClass.java
@@ -3,7 +3,7 @@ package access.local;
 /**
  * Created by zdk on 17-2-2.
  */
-class PackagedClass {
+public class PackagedClass {
     public PackagedClass(){
         System.out.println("Creating a packaged class");
     }
