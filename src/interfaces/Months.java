3523c95d04388ea45e513e7bd07c761d815b98e0
ZouDikai <zdk_cz@sina.com>
2017-02-06 20:16:48 +0800

    to 9.7

---
+++ src/interfaces/Months.java
@@ -0,0 +1,10 @@
+package interfaces;
+
+/**
+ * Created by zdk on 17-2-6.
+ */
+public interface Months {
+    int JANARY = 1, FEBRURAY = 2, MARCH = 3,
+            APRIL = 4, MAY = 5, JUNE = 6, JULY = 7, AUGUST = 7,
+            SEPTEMBER = 9, OCTOBER = 10, NOVEMBER = 11, DECEMBER = 12;
+}
---
+++ src/interfaces/TestRandVals.java
@@ -0,0 +1,13 @@
+package interfaces;
+
+/**
+ * Created by zdk on 17-2-6.
+ */
+public class TestRandVals {
+    public static void main(String[] args){
+        System.out.println(RandVals.RANDOM_INT);
+        System.out.println(RandVals.RANDOM_LONG);
+        System.out.println(RandVals.RANDOM_FLOAT);
+        System.out.println(RandVals.RANDOM_DOUBLE);
+    }
+}
